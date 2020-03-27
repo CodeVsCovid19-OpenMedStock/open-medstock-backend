79ee6687131bcc13586fad62ec8f5b8fd7916a47
Markus Blaser <markus@bachilast.ch>
2020-03-27 22:28:51 +0100

    Added Database connection

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
-from flask import Flask
+#!/usr/bin/python
+
+from flask import * #Flask, render_template, request, redirect
+import pymysql
+from flask_bootstrap import Bootstrap
+from configparser import ConfigParser
+import json, os.path
 
 app = Flask(__name__)
+Bootstrap(app)
 
+
+class Database:
+    def __init__(self):
+        config = ConfigParser()
+        config.read('config.properties')
+        host = config['DatabaseSection']['database.host']
+        user = config['DatabaseSection']['database.user']
+        password = config['DatabaseSection']['database.password']
+        db = config['DatabaseSection']['database.dbname']
+        self.con = pymysql.connect(host=host, user=user, password=password, db=db, cursorclass=pymysql.cursors.DictCursor)
+        self.cur = self.con.cursor()
 
 @app.route('/')
 def hello_world():
