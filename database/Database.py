b6b6d118e4b53f451bd1553cde41b1060ea66312
Markus Blaser <markus@bachilast.ch>
2020-03-29 08:48:54 +0200

    changed reading database information from properties

--- database/Database.py
+++ database/Database.py
@@ -9,16 +9,16 @@ class Database(object):
         config = ConfigParser()
         config.read('./config.properties')
         print("try to connect to the database")
-        host = str(config.get('DatabaseSection', 'database.host'))
+        host = config.get('DatabaseSection', 'database.host')
         port = int(config.get('DatabaseSection', 'database.port'))
-        user = str(config.get('DatabaseSection', 'database.user'))
-        password = str(config.get('DatabaseSection', 'database.password'))
-        db = str(config.get('DatabaseSection', 'database.dbname'))
-        host = "sql7.freemysqlhosting.net"
-        user = "sql7329640"
-        password = "Q6FRAYZr56"
-        db = "sql7329640"
-        print(host, user, password, db)
+        user = config.get('DatabaseSection', 'database.user')
+        password = config.get('DatabaseSection', 'database.password')
+        db = config.get('DatabaseSection', 'database.dbname')
+        #host = "sql7.freemysqlhosting.net"
+        #user = "sql7329640"
+        #password = "Q6FRAYZr56"
+        #db = "sql7329640"
+
         self.__con = pymysql.connect(host=host, user=user, password=password, db=db,
                                    cursorclass=pymysql.cursors.DictCursor)
         print("database successfully connected")
